911bd6e402a06d42b9385d789957814997f81319
Jyotiraditya <jyotiraditya182@gmail.com>
2018-08-25 18:07:24 +0000

    Xtremified

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Phantom Kernel by @Dencel007. Credits: TeamQuantum & TeamGhost
+kernel.string=Xtreme Kernel by @jyotiradityaJP.
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
