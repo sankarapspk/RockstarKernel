44bf5d7881399d53267822fe183454be31df1a85
Dhruvgera <dhruvgera61@gmail.com>
2018-09-23 07:59:06 +0000

    Edits

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Xtreme Kernel by @jyotiradityaJP.
+kernel.string=Rockstar Kernel by @Dhruv007
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
